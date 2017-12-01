22a2bd314b4af1b1b8557a183da5fdc00e4ce8b5
Duo Zhang Alienware <lj19850611@gmail.com>
2017-11-30 21:29:05 -0600

    library
    let's go

--- Decorator/decorator3.py
+++ Decorator/decorator3.py
@@ -2,31 +2,36 @@
 # -*- coding:utf-8 -*-
 # Author: Duo
 user,pwd = 'duo','zd900824'
-def auth(func):
-    def wrapper(*args,**kwargs):
-        username = input("please input username:").strip()# strip() 方法用于移除字符串头尾指定的字符（默认为空格）
-        password = input("please input password:").strip()
+def auth(auth_type):
+    # 检测auth_type是否被传进来了
+    # print('the current authentication method is:%s'%auth_type)
+    def outer_wrapper(func):
+        def wrapper(*args,**kwargs):
+            if auth_type == "local":
+                username = input("please input username:").strip()# strip() 方法用于移除字符串头尾指定的字符（默认为空格）
+                password = input("please input password:").strip()
 
-        if user == username and pwd ==password:
-            print("user has passed the authentication")
-            return func(*args,**kwargs) #写return是为了接收有返回值的函数的返回值
-        else:
-            exit("invalid username or password")
-    return wrapper
+                if user == username and pwd ==password:
+                    print("user has passed the authentication")
+                    return func(*args,**kwargs) #写return是为了接收有返回值的函数的返回值
+                else:
+                    exit("invalid username or password")
+            elif auth_type == "remote":
+                print("this means to do remote authentication")
+                print("welcome to new page")
+        return wrapper
+    return outer_wrapper
 
 
 
 def index():
     print('welcome to index')
-@auth
+@auth(auth_type = "remote")
 def home():
     print('welcome to the users home')
     return "from home"
-@auth
+@auth(auth_type = "remote")
 def bbs():
     print('welcome to bbs page')
-
-
-index()
 
 print(home())
---
+++ Decorator/generatelist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: Duo
+
+# 介绍一种新的列表定义方式 （列表生成式）
+
+# list = [ i*2 for i in range(10)]
+#
+# print(list)
+
+
+# a = []
+# for i in range(10):
+#     a.append(i*2)
+#
+# print(a)
+# 列表生成器是边生成边调用
+generator = (i*2 for i in range(10))
+
+# for i in generator:
+#     print(i)
+# 生成器只有在调用的时候才会执行生成过程。（生成相应的数据）
+# 当生成器由于某种原因终止在某个点的时候我们可以通过调用  __next()__方法来获取下一个应该生成的数据
+
+print(generator.__next__())
+print(generator.__next__())
+print(generator.__next__())
+# 生成器没有办法回到上一个，生成器只记住当前位置，它及不知道前面是什么也不知道后面是什么，
+# 生成器就以这种方式节省内存
+# 对于
